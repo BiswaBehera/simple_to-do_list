568f492ebee2ac9ab601bc25910cb657d6bb0663
Biswajit Behera <behera.biswa5398@gmail.com>
2019-12-05 00:11:46 +0530

    search todos feature

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const addForm = document.querySelector(".add");
 
 const list = document.querySelector(".todos");
 
+const search = document.querySelector(".search input");
+
 
 
 // add todos
@@ -40,5 +42,29 @@ list.addEventListener("click", e => {
         e.target.parentElement.remove();
 
     }
+
+});
+
+// search todos
+const fiterTodo = term => {
+
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => {
+            todo.classList.add("filtered");
+        });
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => {
+            todo.classList.remove("filtered");
+        });
+
+};
+
+search.addEventListener("keyup", () => {
+
+    let term = search.value.trim().toLowerCase();
+    fiterTodo(term);
 
 });
